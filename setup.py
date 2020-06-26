17b030e7c6ac4c528d1002f4811395d123e49bec
Lucas Sulzbach <lucas@sulzbach.org>
2020-06-26 00:22:27 -0300

    setup: Update description, URL and entrypoints

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ setup(
     version="0.0.1",
     author="Lucas Sulzbach",
     author_email="lucas@sulzbach.org",
-    description="Collection of OCR-D compliant tools for layout analysis and segmentation of historical documents from the German-Brazilian Newspapers dataset",
+    description="Collection of OCR-D compliant tools for layout analysis and segmentation of historical german-language documents published in Brazil",
     long_description=open("README.md", "r", encoding='utf-8').read(),
     long_description_content_type="text/markdown",
     keywords='gbn',
     license='Apache',
-    url="https://dokumente.ufpr.br",
+    url="https://github.com/sulzbals/gbn",
     packages=find_packages(exclude=["*.tests", "*.tests.*",
                                     "tests.*", "tests"]),
     install_requires=install_requires,
@@ -23,13 +23,10 @@ setup(
     },
     entry_points={
       'console_scripts': [
-        "ocrd-gbn-mask=gbn:ocrd_gbn_mask",
         "ocrd-gbn-sbb-predict=gbn:ocrd_gbn_sbb_predict",
         "ocrd-gbn-sbb-binarize=gbn:ocrd_gbn_sbb_binarize",
         "ocrd-gbn-sbb-crop=gbn:ocrd_gbn_sbb_crop",
         "ocrd-gbn-sbb-segment=gbn:ocrd_gbn_sbb_segment",
-        "ocrd-gbn-sbb-page-segment=gbn:ocrd_gbn_sbb_page_segment",
-        "ocrd-gbn-sbb-region-segment=gbn:ocrd_gbn_sbb_region_segment",
       ]
     },
     python_requires='>=3.6.0',
