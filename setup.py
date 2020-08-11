34593b6d143776c809aa58c06e18cb306ed63f28
Lucas Sulzbach <lucas@sulzbach.org>
2020-08-11 18:26:28 -0300

    Update package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as fp:
 
 setup(
     name="gbn",
-    version="0.0.1",
+    version="1.0.0",
     author="Lucas Sulzbach",
     author_email="lucas@sulzbach.org",
     description="Collection of OCR-D compliant tools for layout analysis and segmentation of historical german-language documents published in Brazil",
@@ -24,8 +24,8 @@ setup(
     entry_points={
       'console_scripts': [
         "ocrd-gbn-sbb-predict=gbn:ocrd_gbn_sbb_predict",
-        "ocrd-gbn-sbb-binarize=gbn:ocrd_gbn_sbb_binarize",
         "ocrd-gbn-sbb-crop=gbn:ocrd_gbn_sbb_crop",
+        "ocrd-gbn-sbb-binarize=gbn:ocrd_gbn_sbb_binarize",
         "ocrd-gbn-sbb-segment=gbn:ocrd_gbn_sbb_segment",
       ]
     },
